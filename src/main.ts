1ee7b337fe42ab2f00abdef26c2c4f5cf5a7b1ba
tangcuyu <joysmshr@126.com>
2019-09-17 12:01:53 +0800

    init css

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import router from './router/router';
+import store from './store/store';
+import _ from 'lodash';
+
+import './assets/css/common.css';
+import 'animate.css';
+import './assets/css/gisPopupStyle.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+Vue.prototype._ = _;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount('#app');
